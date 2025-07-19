d978d352117464972f2f3abe064f8aab6431cf90
Profac-0 <marlonfalcao5@gmail.com>
2025-07-19 10:27:32 -0300

    Add files via upload

---
+++ scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+
+	Button start;
+	Button exit;
+	public PackedScene start_level = GD.Load<PackedScene>("res://node_2d.tscn");
+
+
+
+	public override void _Ready()
+	{
+	}
+
+	public void _on_start_button_pressed(){
+		GD.Print("Wotakoi");
+		GetTree().ChangeSceneToPacked(start_level);
+	}
+	public void _on_exit_button_pressed(){
+		GD.Print("Romantic Killer");
+		GetTree().Quit();
+	}
+
+}
+
---
+++ scripts/Objects.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Objects : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("ola mundo");
+	}
+
+}
