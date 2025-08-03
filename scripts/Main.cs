6cb900c4542311bfc4dd17ebe570c617fa4f6927
Cassiano <saurinho.64.ss@gmail.com>
2025-08-03 11:56:17 -0300

    Atualizando gitignore

---
+++ scripts/Main.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+
+
+public partial class Node2d : Node2D
+{
+	Node2D player;
+	Button botaoA,botaoB,botaoC,botaoD;
+	Vector2 mouse;
+	bool over = false;
+
+
+
+	public override void _Ready(){
+		player=GetNode<Node2D>("mob");
+		
+	}
+
+	public void _on_colisao_pressed(){
+		GD.Print("Shangri-la");
+		over = true;
+	}
+
+	public void _on_colisao_released(){
+		GD.Print("Boruto");
+		over = false;
+	}
+
+	public void drag_on(){
+		Transform2D position = Transform2D.Identity;
+		position.Origin = GetGlobalMousePosition();
+		player.Transform = position;
+	}
+
+
+
+
+	 /*public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseButton)
+		{
+			if (mouseButton.ButtonIndex == MouseButton.Left && mouseButton.Pressed)
+			{
+				GD.Print("Dr.stone");
+			}
+		}
+	}*/
+
+
+	public override void _Process(double delta)
+	{
+		if(over == true){
+			drag_on();
+		}
+	}
+	
+}
